1f73857f3239a84a2bfc756db0d15022b9d3d8bc
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-11 17:31:43 -0600

    added volume

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/math/calculate', calculate)
 
 app.post('/math/sum', sum)
 
+app.all('/math/volume/:num1/:num2/:num3', (req, res) => {
+  var num1 = parseFloat(req.params.num1)
+  var num2 = parseFloat(req.params.num2)
+  var num3 = parseFloat(req.params.num3)
+  res.send(String(num1 * num2 * num3))
+})
+
 module.exports = {
   app: app
 }
