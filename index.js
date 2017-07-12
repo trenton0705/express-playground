15b8f830eedfaa2445f0e760ea2f4c0d78cfda07
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-07-11 22:10:17 -0600

    added modules to components file

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
-var area = require('./area')
-var calculate = require('./calculate')
-var pi = require('./pi')
-var sum = require('./sum')
-var volume = require('./volume')
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
+
+var area = require('./components/area')
+var calculate = require('./components/calculate')
+var pi = require('./components/pi')
+var sum = require('./components/sum')
+var volume = require('./components/volume')
 
 
 app.get('/math/pi', pi)
@@ -22,8 +22,8 @@ app.all('/math/volume/:num1/:num2/:num3', volume)
 
 app.post('/math/area', area)
 
+app.listen(8080)
+
 module.exports = {
   app: app
 }
-
-app.listen(8080)
